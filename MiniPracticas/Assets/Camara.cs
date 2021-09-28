c690418a419d0791a051ad1f83ef46d09ba09c8d
Alvaro <alvaritoferrandiz@gmail.com>
2021-09-28 12:02:55 +0200

    Movimiento nave
    Perfeccionando el movimiento de la nave y la interactividad de esta

--- MiniPracticas/Assets/Camara.cs
+++ MiniPracticas/Assets/Camara.cs
@@ -8,6 +8,9 @@ public class Camara : MonoBehaviour
     [SerializeField] float smoothTime = 0.3f;
     private Vector3 velocity = Vector3.zero;
 
+    float startTime = 0f;
+    float holdTime = 5.0f;
+
     // Start is called before the first frame update
 
     void Start()
@@ -21,6 +24,25 @@ public class Camara : MonoBehaviour
         Vector3 targetPosition = playerPosition.TransformPoint(new Vector3(-6, 2, 0));
 
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-        
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startTime = Time.time;
+
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (startTime + holdTime >= Time.time)
+            {
+                targetPosition = new Vector3(-20, 2, 0);
+            }
+        }
+        else
+        {
+            targetPosition = new Vector3(-6, 2, 0);
+        }
+
     }
 }
--- MiniPracticas/Assets/Player.cs
+++ MiniPracticas/Assets/Player.cs
@@ -7,23 +7,35 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
 
     public float speed = 10f;
+    public float dashSpeed = 40f;
 
     //[SerializeField] Vector3 playerPosition = new Vector3(0, 0, 0);
     void Start()
     {
-    
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float translationX = Input.GetAxis("Vertical") * speed;
+        float translationY = Input.GetAxis("Vertical") * speed;
         float translationZ = Input.GetAxis("Horizontal") * speed;
 
-        translationX *= Time.deltaTime;
+        translationY *= Time.deltaTime;
         translationZ *= Time.deltaTime;
 
-        transform.Translate(translationX, 0, 0);
+        transform.Translate(0, translationY, 0);
         transform.Translate(0, 0, translationZ);
+
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0, 0, translationZ * 2);
+        }
+
     }
+
+
 }
+
+   
